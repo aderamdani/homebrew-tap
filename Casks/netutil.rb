4c5ec6c0de1c42ee795648ebbbb4701b6863b2eb
aderamdani <mr.aderamdani@gmail.com>
2026-09-12 19:05:07 +0700

    netutil: add cask 4.14.0

---
+++ Casks/netutil.rb
@@ -0,0 +1,32 @@
+cask "netutil" do
+  version "4.14.0"
+  sha256 "6f4b1f0f308a293e5ece6d189cb5934bee1ef846d9cd8a149253bdac4fd8cae5"
+
+  url "https://github.com/aderamdani/NetUtil/releases/download/v#{version}/NetUtil-#{version}.dmg",
+      verified: "github.com/aderamdani/NetUtil/"
+  name "NetUtil"
+  desc "Native macOS network diagnostics toolkit"
+  homepage "https://github.com/aderamdani/NetUtil"
+
+  depends_on arch: :arm64
+  depends_on macos: ">= :tahoe"
+
+  app "NetUtil.app"
+
+  uninstall quit: "Vertex-Data.NetUtil"
+
+  zap trash: [
+    "~/Library/Preferences/Vertex-Data.NetUtil.plist",
+    "~/Library/Caches/Vertex-Data.NetUtil",
+    "~/Library/HTTPStorages/Vertex-Data.NetUtil",
+  ]
+
+  caveats <<~EOS
+    NetUtil is ad-hoc signed and not notarized, so Gatekeeper blocks the first
+    launch. After installing, clear the quarantine flag:
+
+      xattr -dr com.apple.quarantine "#{appdir}/NetUtil.app"
+
+    or right-click NetUtil in Applications and choose Open.
+  EOS
+end
