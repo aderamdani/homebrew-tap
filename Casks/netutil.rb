18e940fab50e5e3ec6e844dc8d01aade9132c952
aderamdani <mr.aderamdani@gmail.com>
2026-09-14 12:30:46 +0700

    netutil 4.18.0

--- Casks/netutil.rb
+++ Casks/netutil.rb
@@ -1,6 +1,6 @@
 cask "netutil" do
-  version "4.17.0"
-  sha256 "f3dd9f1a940ab2bfd522d62c57ae22b5429b63d9750ff28cc014ba5ef416c3ce"
+  version "4.18.0"
+  sha256 "a04a7bcc65ed6f3776212edd34d947a1d48ce89cb6bd2bc047b9e829b79ea19f"
 
   url "https://github.com/aderamdani/NetUtil/releases/download/v#{version}/NetUtil-#{version}.dmg"
   name "NetUtil"
